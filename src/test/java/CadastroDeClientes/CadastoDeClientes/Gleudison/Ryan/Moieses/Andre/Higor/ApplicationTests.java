3b5e27d85239d03a9517b883182d0e96e686fbad
HCfarias <higorcarvalhoffcial1@gmail.com>
2022-09-19 16:28:23 -0300

    Primeiro

---
+++ src/test/java/CadastroDeClientes/CadastoDeClientes/Gleudison/Ryan/Moieses/Andre/Higor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package CadastroDeClientes.CadastoDeClientes.Gleudison.Ryan.Moieses.Andre.Higor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
